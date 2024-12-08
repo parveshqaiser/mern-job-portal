53483d9995e18d9b7f7543ada0b3bbb1d9af89b5
Parvesh Qaiser <parvesh@entrolabs.com>
2024-12-08 23:56:33 +0530

    api changed to production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 
-export const commonEndPoints = "http://127.0.0.1:5000";
-// export const commonEndPoints = "https://mern-job-portal-backend-k5o0.onrender.com";
+// export const commonEndPoints = "http://127.0.0.1:5000";
+export const commonEndPoints = "https://mern-job-portal-backend-k5o0.onrender.com";
 
 export const headers = {
    "Content-type": "application/json", // Content-Type should be in lowercase
